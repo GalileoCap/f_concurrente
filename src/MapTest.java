ea0a2b7b463fa2c9047cdb86fa797faf7e23364f
GalileoCap <galileocapp@gmail.com>
2023-05-16 09:17:54 -0300

    OK: MapTest

--- src/MapTest.java
+++ src/MapTest.java
@@ -5,8 +5,97 @@ import Map.Map;
 
 class MapTest extends Test {
   public static void run() {
-    printCase("Map", "ing");
-    success();
+    testFindNull();
+    testAddAndFind();
+    testAddAndFindMany();
+    testRemove();
+    testFindAfter();
+  }
+
+  static void testFindNull() {
+    printCase("Map", "findNull");
+
+    Map<String> m = new Map<String>();
+    String s = m.find(0);
+
+    Assert(s == null);
+
+    Success();
+  }
+
+  static void testAddAndFind() {
+    printCase("Map", "addAndFind");
+
+    Map<String> m = new Map<String>();
+    String s = "Ahoy there!";
+    int key = s.hashCode();
+    
+    m.add(s);
+
+    Assert(m.find(key) == s, "Couldn't find"); 
+    Assert(m.find(key+1) == null, "Found another key");
+
+    Success();
+  }
+
+  static void testAddAndFindMany() {
+    printCase("Map", "addAndFindMany");
+
+    Map<String> m = new Map<String>();
+    String s0 = "Ahoy there!";
+    String s1 = "My friend!";
+    int k0 = s0.hashCode();
+    int k1 = s1.hashCode();
+    
+    m.add(s0);
+    m.add(s1);
+
+    Assert(m.find(k0) == s0, "Couldn't find s0"); 
+    Assert(m.find(k1) == s1, "Couldn't find s1");
+
+    Success();
+  }
+
+  static void testRemove() {
+    printCase("Map", "addAndRemove");
+
+    Map<String> m = new Map<String>();
+    String s = "Ahoy there!";
+    int key = s.hashCode();
+    
+    m.add(s);
+    m.remove(key);
+
+    Assert(m.find(key) == null, "Still found s"); 
+
+    Success();
+  }
+
+ static void testFindAfter() {
+    printCase("Map", "findAfter");
+
+    class Item {
+      int hash;
+      public Item(int _hash) {
+        hash = _hash;
+      }
+
+      public int hashCode() {
+        return hash;
+      }
+    }
+
+    Map<Item> m = new Map<Item>();
+    Item i0 = new Item(0);
+    Item i1 = new Item(1);
+    
+    m.add(i0);
+    m.add(i1);
+
+    Assert(m.findAfter(0) == i1, "Couldn't find i1"); 
+    Assert(m.findAfter(1) == null, "Found another element");
+
+    Success();
   }
 }
 
--- src/Test.java
+++ src/Test.java
@@ -11,21 +11,47 @@ class RunTests {
 class Test {
   //public static void run();
 
-  public static void printCase(String suiteName, String caseName) {
-    System.out.print(String.format("%s::%s", suiteName, caseName));
+  static void printCase(String suiteName, String caseName) {
+    System.err.print(String.format("%s::%s", suiteName, caseName));
   }
 
-  public static void success() {
-    System.out.println(" SUCCESS");
+  static void Success(String msg) {
+    System.err.println(String.format(" SUCCESS: %s", msg));
+  }
+  static void Success() {
+    System.err.println(" SUCCESS");
   }
 
-  public static void error() {
-    System.out.println(" ERROR");
+  static void Error(String msg) {
+    System.err.println(String.format(" ERROR: %s", msg));
+  }
+  static void Error() {
+    System.err.println(" ERROR");
   }
 
-  public static void fail() {
-    System.out.println(" FAIL");
+  static void Fail(String msg) {
+    System.err.println(String.format(" FAIL: %s", msg));
     System.exit(1);
+  }
+  static void Fail() {
+    System.err.println(" FAIL");
+    System.exit(1);
+  }
+
+  static Boolean Expect(Boolean cond, String msg) {
+    if (!cond) Error(msg);
+    return cond;
+  }
+  static Boolean Expect(Boolean cond) {
+    if (!cond) Error();
+    return cond;
+  }
+
+  static void Assert(Boolean cond, String msg) {
+    if (!cond) Fail(msg);
+  }
+  static void Assert(Boolean cond) {
+    if (!cond) Fail();
   }
 }
 
