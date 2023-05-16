306d908efc94b16d03b7884b87e1fda0f3530fe9
GalileoCap <galileocapp@gmail.com>
2023-05-16 08:00:15 -0300

    OK: newPost and removePost

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,18 @@ class Main {
   public static void main(String args[]) {
     Twiner t = new Twiner();
 
-    int sessionId = t.logIn(0);
-    System.out.println(sessionId);
-    System.out.println(t.logOut(1, sessionId));
-    System.out.println(t.logOut(0, 99));
-    System.out.println(t.logOut(0, sessionId));
-    System.out.println(t.logOut(0, sessionId));
+    int s0 = t.logIn(0);
+    int s1 = t.logIn(1);
+
+    int post = t.newPost(0, s0);
+    t.removePost(post, 2, 0);
+    t.removePost(post, 1, s0);
+  
+    t.logOut(0, s0);
+    s0 = t.logIn(0);
+
+    System.out.println(t.removePost(post, 0, s0));
+    //System.out.println(sessionId);
   }
 }
 
--- src/Twiner.java
+++ src/Twiner.java
@@ -16,22 +16,47 @@ class User {
   }
 }
 
+class Post {
+  public int ownerId;
+  public int date;
+
+  public Post(int _ownerId, int _date) {
+    ownerId = _ownerId;
+    date = _date;
+  }
+
+  public int hashCode() {
+    return date;
+  }
+}
+
 public class Twiner {
   private Map<User> loggedIn;
+  private Map<Post> posts;
 
   private int lastSessionId;
+  private int date;
 
   public Twiner() {
-    lastSessionId = 1;
+    lastSessionId = 0;
     loggedIn = new Map<User>();
+
+    date = 0;
+    posts = new Map<Post>();
+  }
+  
+  private Boolean checkUser(int userId, int sessionId) {
+    User user = loggedIn.find(userId);
+    return user != null && user.sessionId == sessionId;
   }
 
   public int logIn(int userId) {
     User user = new User(userId, lastSessionId);
-    if (loggedIn.add(user)) { // Success
-      lastSessionId++;
-      return user.sessionId;
-    } else return 0; // Already logged in
+    if (!loggedIn.add(user)) // Already logged in
+      return -1;
+
+    lastSessionId++;
+    return user.sessionId;
   }
 
   public Boolean logOut(int userId, int sessionId) {
@@ -40,5 +65,27 @@ public class Twiner {
       return false;
 
     return loggedIn.remove(userId);
+  }
+
+  public int newPost(int userId, int sessionId) {
+    if (!checkUser(userId, sessionId))
+      return -1;
+
+    Post post = new Post(userId, date++);
+    if (!posts.add(post))
+      return -1;
+
+    return post.date;
+  }
+
+  public Boolean removePost(int date, int userId, int sessionId) {
+    if (!checkUser(userId, sessionId))
+      return null;
+
+    Post post = posts.find(date);
+    if (post.ownerId != userId)
+      return false;
+
+    return posts.remove(date);
   }
 }
