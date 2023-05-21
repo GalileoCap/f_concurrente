27177cc91aaeca2e04b4d1d8aef0bf56dc03e2f4
GalileoCap <galileocapp@gmail.com>
2023-05-21 14:13:20 -0300

    TRY: Less cases

--- analyze/utils.py
+++ analyze/utils.py
@@ -32,13 +32,16 @@ def joinRanges(*ranges):
 
 def testRanges():
   modes = ['free', 'monitor']
-  rangeLogIn, rangeLogOut, rangeNewPost, rangeNextPost, rangeRemovePost = [range(1, 3+1)] * 5
+  rangeLogIn, rangeLogOut, rangeNextPost = [range(1, 3+1)] * 3
+  rangeNewPost, rangeRemovePost = ([0], [0])
   rangeActions = range(1, 5+1)
   return (modes, rangeLogIn, rangeLogOut, rangeNewPost, rangeNextPost, rangeRemovePost, rangeActions)
 
 def smallRanges():
-  modes = ['free', 'lazy', 'optimistic', 'fine-grained', 'monitor']
-  rangeLogIn, rangeLogOut, rangeNewPost, rangeNextPost, rangeRemovePost = [range(1, 10+1)] * 5
+  modes = ['free', 'optimistic', 'fine-grained']
+  # rangeLogIn, rangeLogOut, rangeNewPost, rangeNextPost, rangeRemovePost = [range(1, 10+1)] * 5
+  rangeLogIn, rangeLogOut, rangeNextPost = [range(1, 10+1)] * 3
+  rangeNewPost, rangeRemovePost = ([0], [0])
   rangeActions = range(1, 10+1)
   return (modes, rangeLogIn, rangeLogOut, rangeNewPost, rangeNextPost, rangeRemovePost, rangeActions)
 
