0cc670aae6ab5ce6a8c474eac27b16257dc88808
leomansini <leomansini2000@gmail.com>
2023-05-22 23:13:50 -0300

    save to pandas

--- analyze/cases.py
+++ analyze/cases.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 import utils
 from utils import log, DATADIR, BUILDDIR
+from statistics import median
+from tqdm import tqdm
 
 def executeCase(mode, logIn, logOut, newPost, nextPost, removePost, actions):
   cmd = f'java -cp {BUILDDIR} ThreadPool {mode} {logIn} {logOut} {newPost} {nextPost} {removePost} {actions}'
@@ -21,42 +23,32 @@ def processCaseOutput(stdout):
     mode = out[0]
     times = [int(t) for t in out[1:]]
     data[mode] = data.get(mode, []) + times
+  for mode, times in data.items():
+    data[mode] = median(times)
   return data
 
 def getCaseFpath(mode, logIn, logOut, newPost, nextPost, removePost, actions):
   return os.path.join(DATADIR, f'{mode}_{logIn}_{logOut}_{newPost}_{nextPost}_{removePost}_{actions}.json')
 
-CACHE = {} # keys are 'mode|logIn|logOut|newPost|nextPost|removePost|actions' separated by '|'
-
-def get_cache_key(case):
-  return "|".join(case)
-
-def runCase(case, force = False):
-  # If this case has already been executed and saved, load it
-  key = get_cache_key(case)
-  if (not force) and (key in CACHE):
-      return CACHE[key]
+def runCase(case):
 
   log('[runCase] RUN', case, level = 'deepDebug')
   stdout = executeCase(*case)
   data = processCaseOutput(stdout)
-  try:
-    data = pd.DataFrame(data)
-  except:
-    breakpoint()
-
-  CACHE[key] = data
+  data = pd.DataFrame(data, index=[0])
+  data["mode"] = case[0]
 
   return data
 
-def runAllCases(ranges, force = False):
+def runAllCases(ranges):
   log('[runAllCases]', level = 'user')
 
   os.makedirs(DATADIR, exist_ok = True)
 
   data = pd.DataFrame()
-  for case in itt.product(*ranges):
-    data = pd.concat([data, runCase(case, force)])
+  cases = list(itt.product(*ranges))
+  for case in tqdm(cases):
+    data = pd.concat([data, runCase(case)])
 
   data.to_csv("data.csv", index=False)
 
