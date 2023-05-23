7c862991a14131ebe53fd1dca8e37c73f5d69989
leomansini <leomansini2000@gmail.com>
2023-05-22 23:14:03 -0300

    start making ranges requested in pdf

--- analyze/utils.py
+++ analyze/utils.py
@@ -66,6 +66,23 @@ def fullRanges(): # ~full
   rangeActions = joinRanges(range(1, 10), range(10, 100, 10), range(100, 1000, 100), range(1000, 10000, 1000))
   return (modes, rangeLogIn, rangeLogOut, rangeNewPost, rangeNextPost, rangeRemovePost, rangeActions)
 
+# Propuestos en la consigna
+
+# la variación en la proporción de hilos que ejecutan determinadas operaciones, manteniendo constante la cantidad de hilos totales. 
+def ranges1():
+  modes = ['free', 'lazy', 'optimistic', 'fine-grained', 'monitor']
+  rangeLogIn, rangeLogOut, rangeNewPost, rangeNextPost, rangeRemovePost = (
+    joinRanges(range(1, 100, 5), range(100, 1000, 100)),
+    joinRanges(range(1, 100, 5), range(100, 1000, 100)),
+    joinRanges(range(1, 100, 5), range(100, 1000, 100)),
+    joinRanges(range(1, 100, 5), range(100, 1000, 100)),
+    joinRanges(range(1, 100, 5), range(100, 1000, 100))
+  )
+  rangeActions = joinRanges(range(1, 10), range(10, 100, 10), range(100, 1000, 100), range(1000, 10000, 1000))
+  return (modes, rangeLogIn, rangeLogOut, rangeNewPost, rangeNextPost, rangeRemovePost, rangeActions)
+
+
+
 Ranges = {
   'test': testRanges(),
   'small': smallRanges(),
