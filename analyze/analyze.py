bd99c375377438b14657738c6abbe32a0386b21f
GalileoCap <galileocapp@gmail.com>
2023-05-28 10:25:37 -0300

    OK: Merged notebook into analyze.py

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -6,41 +6,32 @@ import utils
 from utils import log, OUTDIR
 
 def analyze(df, name):
-  # print(df.describe(), df.columns, sep = '\n')
+  cols = ['mode', 'actions', 'logIn_threads', 'logOut_threads', 'apiRequest_threads']
+  df = df.groupby(cols).mean().reset_index()
+  print(df.head())
 
   df['totalThreads'] = df['logIn_threads'] + df['logOut_threads'] + df['apiRequest_threads']
 
-  data = []
-  for totalThreads in range(df['totalThreads'].min(), df['totalThreads'].max()+1):
-    for currentThreads in range(df['logIn_threads'].min(), df['logIn_threads'].max()+1): # NOTE: Assumes all types have the same range
-      for current in ['logIn_threads', 'logOut_threads', 'apiRequest_threads']:
-        _df = df[
-          (df['totalThreads'] == totalThreads) &
-          (df[current] == currentThreads)
-        ]
-        if len(_df) > 0:
-          stats = _df[['logIn_meanTime', 'logOut_meanTime', 'apiRequest_meanTime', 'totalTime']].agg(['mean', 'std'])
-          _data = {'op': current, 'totalThreads': totalThreads, 'opThreads': currentThreads}
-          for idx, row in stats.iterrows():
-            for col, value in row.items():
-              _data[f'{col}_{idx}'] = value
-          data.append(_data)
+  for op in ['logIn', 'logOut', 'apiRequest']:
+    xaxis, zaxis, color = f'{op}_threads', 'totalThreads', 'mode'
+    fpath = os.path.join(OUTDIR, f'{op}.html')
+    foo(df, xaxis, f'{op}_meanTime', zaxis, color, fpath)
+    # foo(df, xaxis, 'totalTime', zaxis, color, fpath)
 
-  df = pd.DataFrame(data)
-  print(df.describe())
+def foo(df, xaxis, yaxis, zaxis, color, fpath):
+  dfNoDup = df[[xaxis, yaxis, zaxis, color]].drop_duplicates() # TODO: Why this?
   fig = px.scatter(
-    df,
-    x = 'opThreads',
-    y = 'totalTime_mean',
-    animation_frame = 'totalThreads',
+    dfNoDup,
+    x = xaxis,
+    y = yaxis,
+    animation_frame = zaxis,
     # animation_group = 'op',
-    color = 'op',
+    color = color,
 
     log_y = True,
-    range_x = [df['opThreads'].min()-1, df['opThreads'].max()+1],
-    range_y = [df['totalTime_mean'].min(), df['totalTime_mean'].max()],
+    range_x = [dfNoDup[xaxis].min()-1, dfNoDup[xaxis].max()+1],
+    range_y = [dfNoDup[yaxis].min(), dfNoDup[yaxis].max()],
   )
 
   os.makedirs(OUTDIR, exist_ok = True)
-  fpath = os.path.join(OUTDIR, 'anim.html')
   fig.write_html(fpath)
